8023e2d2720dbfc9b28ec40253e2dc80a169dbc0
Juan Ayala <juanayala099@gmail.com>
2022-08-07 15:42:36 -0400

    fetching and showed info, first deploy

---
+++ index.js
@@ -0,0 +1,31 @@
+const OPTIONS = {
+	method: 'GET',
+	headers: {
+        'X-RapidAPI-Key': 'be3d54d51amshd1a30175d9b9666p122037jsnbd48772bd236',
+        'X-RapidAPI-Host': 'ip-geolocation-and-threat-detection.p.rapidapi.com'
+      }
+};
+
+
+const fetchIpInfo = ip => {
+    return fetch(`https://ip-geolocation-and-threat-detection.p.rapidapi.com/${ip}`, OPTIONS)
+    .then (res => res.json())
+    .catch(err => console.error(err))
+}
+
+const form = document.querySelector("#form")
+const input = document.querySelector("#input")
+const submit = document.querySelector("#submit")
+const results = document.querySelector("#results")
+
+form.addEventListener("submit", async (e)=>{
+    e.preventDefault()
+    const {value} = input
+    if(!value) return
+    const ipInfo = await fetchIpInfo(value)
+
+
+    if (ipInfo){
+        results.innerHTML = JSON.stringify(ipInfo, null, 2)
+    }
+})
